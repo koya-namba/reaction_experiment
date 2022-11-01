3c7a3ea37ed7e5ac5b26638ae1582d59e5e03cc9
koya-namba <namba.koya@arakawa-lab.com>
2022-11-01 13:53:02 +0900

    delete pyaudio

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ import controllers.main as main
 # 作業中かを判定する時間
 AFK_THRESHOLD = 5
 
-# 話しているかを判定する音量
-AUDIO_THRESHOLD = 2000
-
 # CSVにデータを書き込む時間間隔
 WRITE_CYCLE = 1
-
 
 ##################################################
 
 
 if __name__ == "__main__":
-        main.main(AFK_THRESHOLD, AUDIO_THRESHOLD, WRITE_CYCLE)
+        main.main(AFK_THRESHOLD, WRITE_CYCLE)
